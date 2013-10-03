cb9a71414cd041e7de2448c3e3bec69cafeb29aa
bonnerl <bonnerl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-03 17:22:47 +0000

    0.9.0 Beta: Improved the inline documentation of cache.php according to the PHP Documentation Standards.
    git-svn-id: https://plugins.svn.wordpress.org/facebook-feed-grabber/branches/0.9.0@782042 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- caching.php
+++ caching.php
@@ -1,19 +1,46 @@
 <?php
 
-/* - - - - - -
-	
-	Class for caching/retrieving requests.
-	
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/**
+ * Class for caching/retrieving requests.
+ * 
+ * Contains methods and properties for caching the results from
+ * Facbook.
+ * 
+ * @since 0.7
+ */
 class ffg_cache {
 	
-	// Folder to cache files to.
+	/**
+	 * Folder to cache files to.
+	 * 
+	 * @since 0.7
+	 * @access public
+	 * @var string The folder to store the cache in.
+	 */
 	public $folder = null;
 	
-	// Prefix for file names.
+	/**
+	 * Prefix for file names.
+	 * 
+	 * @since 0.7
+	 * @access public
+	 * @var string Prefix for the cache's file names.
+	 */
 	public $prefix = 'ffg-';
 	
+	/**
+	 * Instantiate the class.
+	 * 
+	 * Get's the plugin's options and stores the folder.
+	 * 
+	 * @since 0.7
+	 * @return void Doesn't return anything special.
+	 */
 	function __construct(  ) {
+
+		/*
+			LBTD: Make this use the new options retrieval method.
+		*/
 		
 		// Get stored plugin options
 		$options = get_option('ffg_options');
@@ -23,11 +50,21 @@ class ffg_cache {
 		
 	}
 	
-	/* - - - - - -
-		
-		Does the whole process.
-		
-	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+	/**
+	 * Does the whole process.
+	 * 
+	 * Checks to see if the requested conent is cached,
+	 * gets the content whether cached or not.
+	 * saves new content to the cache.
+	 * 
+	 * @since 0.7
+	 * 
+	 * @param object $fb A Facebook class object.
+	 * @param string $file The Facebook content to get.
+	 * @param string $expires Cache expiration as a unix timestamp.
+	 * 
+	 * @return string The content.
+	 */
 	function theMagic( $fb, $file, $expires ) {
 			
 		// Check if it has already been cached and not expired
@@ -43,21 +80,36 @@ class ffg_cache {
 		return $content;
 	}
 	
-	/* - - - - - -
-		
-		Get the file name.
-		
-	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+	/**
+	 * Get the file name.
+	 * 
+	 * Generates and returns the file name to use 
+	 * for the cached content.
+	 * 
+	 * @since 0.7
+	 * 
+	 * @param string $file A base name for the cached file.
+	 * 
+	 * @return string The file name to be used for caching.
+	 */
 	function getName( $file ) {
 		return $this->prefix . md5($file) . ".json";
 	}
 	
 	
-	/* - - - - - -
-		
-		Check to see if a file is cached
-		
-	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+	/**
+	 * Check to see if a file is cached
+	 * 
+	 * Checks to see if a file has been cached. 
+	 * Returns true if it's found and false if it's not.
+	 * 
+	 * @since 0.7
+	 * 
+	 * @param string $file The base file name used for the cached file.
+	 * @param string $expires Cache expiration as a unix timestamp.
+	 * 
+	 * @return boolean If file is cached. 
+	 */
 	function isCached( $file, $expires ) {		
 		
 		// Get file path/name.
@@ -71,11 +123,17 @@ class ffg_cache {
 	}
 	
 	
-	/* - - - - - -
-		
-		Get a file
-		
-	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+	/**
+	 * Get a file
+	 * 
+	 * Get the cached file.
+	 * 
+	 * @since 0.7
+	 * 
+	 * @param string $file The base file name used for the cached file.
+	 * 
+	 * @return string The contents of the cached file.
+	 */
 	function getCache( $file ) {
 		$cache_file = $this->folder . $this->getName($file);
 		
@@ -83,11 +141,20 @@ class ffg_cache {
 	}
 	
 	
-	/* - - - - - -
-		
-		Caches a file
-		
-	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+	/**
+	 * Caches a file
+	 * 
+	 * Caches the file if the cache directory is there and writable.
+	 * If the directory is not there it tries to make it. 
+	 * Returns TRUE on success and FALSE on failure.
+	 * 
+	 * @since 0.7
+	 * 
+	 * @param string $file The base file name used for the cached file.
+	 * @param string $content The contents to cache.
+	 * 
+	 * @return boolean Results of it's endeavor. 
+	 */
 	function saveCache($file, $content) {
 		
 		$cache_file = $this->folder . $this->getName($file);
